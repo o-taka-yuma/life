b3d23180816dbd9bb997cedea7b7b15dceea875b
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-05-27 23:29:41 +0900

    app.recordのコメントアウトを消した

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,31 @@
 class MainController < ApplicationController
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   
   def index
   end
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
