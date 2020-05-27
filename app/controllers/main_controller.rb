bb2a31dcc3ee53870ad391acc1132546f48ac8aa
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-05-27 23:14:47 +0900

    rails db:migrate

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,8 @@
 class MainController < ApplicationController
 
-
+  
   def index
   end
 
 
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
-root "start#index"
-resources :main, only:[:index]
+  devise_for :users
+  root "start#index"
+  resources :main, only:[:index]
+
+
+  
 end
