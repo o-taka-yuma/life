64354cdb3241f9839aceccde8c52d03ba539685e
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-05-28 00:04:36 +0900

    devise_view_change

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,13 +1,15 @@
 class MainController < ApplicationController
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   
   def index
   end
 
 
 
+  
+  
+
+  
 
 
 
@@ -21,11 +23,5 @@ class MainController < ApplicationController
 
 
 
-
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+  
 end
