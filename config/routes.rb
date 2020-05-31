a9205026068d4134eb0cec9fc7676310f9e3c042
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-06-01 07:48:21 +0900

    Revert "nest"
    This reverts commit 137750adec92af4c46ed55d69760d8f6e181017d.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users
   root "start#index"
-  resources :users do 
-    resources :action_records, only: [:index, :new, :create] 
-  end
+  resources :users
+
+  resources :action_records, only: [:index, :new, :create] 
+  
 end
