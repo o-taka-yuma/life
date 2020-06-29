afd05582522d69c9edad597e2be7b44d1c266811
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-06-30 00:54:25 +0900

    target_rooting

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,7 +1,27 @@
 class TargetsController < ApplicationController
 
+  def new
+    @target = Target.new
+  end
 
+  def create
+    @target = Target.new(target_params)
+    if @target.save
+      redirect_to action_records_path
+    else
+      render :new
+    end
+  end
 
-
+  def edit
+  end
   
+  def update
+  end
+
+
+  private
+  def target_params
+    params.require(:target).permit(:life_goal, :dream, :achivement_taget).merge(user_id: current_user.id)
+  end
 end
