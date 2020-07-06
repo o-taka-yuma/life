149082ba4318a1c9e960c284e6c16e36bf876673
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-07-06 15:08:35 +0900

    css

--- app/models/action_record.rb
+++ app/models/action_record.rb
@@ -1,11 +1,19 @@
 class ActionRecord < ApplicationRecord
   validates :sleep, :work, :takeBreak, presence: true
-  validates :total, presence: true
   validates :recordDay, uniqueness: { scope: :user_id}
+  # validates :total_hour_24
+  
   belongs_to :user
 
   before_save do
     self.total = sleep + work + takeBreak + effort + challenge + chores
   end
+
+  # def total_hour_24
+  #   self.total = sleep + work + takeBreak + effort + challenge + chores
+  #   if total != 24 
+  #     errors.add(:sleep, "合計が24時間になるようにしてください")
+  #   end
+  # end
 
 end
